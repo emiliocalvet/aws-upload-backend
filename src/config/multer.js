617a0b4440c53ac8bf717415a60834c5b7f4b09c
Emilio de Aquino Calvet <emiliocalvet@gmail.com>
2020-12-21 01:14:13 -0300

    first commit

---
+++ src/config/multer.js
@@ -0,0 +1,60 @@
+const multer = require('multer')
+const path = require('path')
+const crypto = require('crypto')
+const aws = require('aws-sdk')
+const multerS3 = require('multer-s3')
+
+const storageTypes = {
+  local: multer.diskStorage({
+    destination: (_req, _file, cb) => {
+      cb(null, path.resolve(__dirname, '..', '..', 'temp', 'uploads'))
+    },
+    filename: (_req, file, cb) => {
+      crypto.randomBytes(16, (err, hash) => {
+        if (err) cb(err)
+
+        file.key = `${hash.toString('hex')}-${file.originalname}`
+
+        cb(null, file.key)
+      })
+    }
+  }),
+
+  s3: multerS3({
+    s3: new aws.S3(),
+    bucket: process.env.BUCKET_NAME,
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    acl: 'public-read',
+    key: (_req, file, cb) => {
+      crypto.randomBytes(16, (err, hash) => {
+        if (err) cb(err)
+
+        file.key = `${hash.toString('hex')}-${file.originalname}`
+
+        cb(null, file.key)
+      })
+    }
+  })
+}
+
+module.exports = {
+  dest: path.resolve(__dirname, '..', '..', 'temp', 'uploads'),
+  storage: storageTypes[process.env.STORAGE_TYPE],
+  limits: {
+    fileSize: 10 * 1024 * 1024
+  },
+  fileFilter: (_req, file, cb) => {
+    const allowedMimes = [
+      'image/jpeg',
+      'image/pjpeg',
+      'image/png',
+      'image/gif' 
+    ]
+
+    if (allowedMimes.includes(file.mimetype)) {
+      cb(null, true)
+    } else {
+      cb(new Error('Invalid file type.'))
+    }
+  }
+}
